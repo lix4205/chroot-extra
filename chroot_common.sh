d7c4639db808be6a5dd0275ab4cca899a197a1d1
lix4205 <35744171+lix4205@users.noreply.github.com>
2018-01-28 14:58:15 +0100

    Update chroot_common.sh

--- chroot_common.sh
+++ chroot_common.sh
@@ -137,4 +137,23 @@ arch_chroot () {
 #}
 
 declare -A to_mount
+
+[[ -z "$1" ]] && die "No directory specified !"
+# On vérifie que ce n'est pas "/" puis qu'on est bien sur un système...
+if [[ -e "$1" ]] && [[ "$1" != "/" ]]; then
+	if [[ -e "$1/bin" ]] && [[ -e "$1/dev" ]] && [[ -e "$1/proc" ]] && [[ -e "$1/sys" ]] && [[ -e "$1/run" ]] && [[ -e "$1/tmp" ]]; then 
+		[[ -d $1/bin ]] && PATH=$PATH:/bin:/sbin:/usr/sbin
+		msg_info -n "Setup chroot to \"$1\"\r"
+		init_chroot "$1" || die "Failed to setup chroot in /"
+		msg_info "Setup chroot to \"$1\"...ok"
+		msg_info "Chrooting to \"$1\""
+		chroot "$1"
+	else
+		die "Unable to find a valid system in \"$1\"!"
+	fi
+else
+	die "\"$1\" is not valid directory !"
+fi
+
+
 # END
